3807da7b206d9041e1602e691ec4330bb5edcb2f
Scott Robb <jscottrobb@yahoo.com>
2017-10-22 23:08:44 -0600

    Greet new users

--- server/server.js
+++ server/server.js
@@ -27,18 +27,28 @@ io.on('connection', (socket) => {
   //    console.log('Created email',email);
   //  });
 
-  // socket.emit('newMessage', {
-  //   from: 'John',
-  //   to: 'Kathy',
-  //   text: 'Want to go to a movie',
-  //   createdAt: 123
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat room',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+      from: 'Admin',
+      text: 'New user in the chat room',
+      createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage',{
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
     });
   });
 
